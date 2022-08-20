52c5570350a1c968599034ce1439166d31df047a
Doug Ransom <doug.ransom@gmail.com>
2022-08-20 13:44:19 -0700

    Added stuff to pyproject

--- src/witertools/__init__.py
+++ src/witertools/__init__.py
@@ -1,4 +1,7 @@
-"""Doc"""
+"""Witertools - Tools to wrap iterator functions, to consume the whole iterator and return a list.  Avoid sprinklist list(...)
+throughout your code when you need a list instead of an interator.  
+
+"""
 __version__="0.0.0"
 
 from .iterator_to_list import iterator_to_list_function,iterator_name_to_list_function
