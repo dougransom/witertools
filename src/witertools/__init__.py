6b2f30c257039ac1f5a9829a3672209e992f8ad7
Doug Ransom <doug.ransom@gmail.com>
2022-08-20 15:11:36 -0700

    docs

--- src/witertools/__init__.py
+++ src/witertools/__init__.py
@@ -4,4 +4,11 @@ throughout your code when you need a list instead of an interator.
 """
 __version__="0.0.0"
 
+import builtins
 from .iterator_to_list import iterator_to_list_function,iterator_name_to_list_function
+import sys
+import builtins as b
+built_in_iterators=["filter","map","zip"]
+this_module=sys.modules[__name__]
+for fn in built_in_iterators:
+    setattr(this_module,fn,iterator_name_to_list_function(b,fn))
